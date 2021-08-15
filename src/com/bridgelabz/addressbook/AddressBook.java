fd33e39b1084e3ce919015bee0a377c89551f65e
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-15 16:25:43 +0530

    [kshitija]:second usecase solved

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -7,13 +7,29 @@ public class AddressBook {
 	String address;
 	String city;
 	String state;
-	String zip;
+	int zip;
 	int phoneNo;
 	String emailId;
+	public AddressBook(String firstName,String lastName,String address,String city,String state,int zip,int phoneNo,String emailId) {
+		this.firstName= firstName;
+		this.lastName= lastName;
+		this.address= address;
+		this.city= city;
+		this.state= state;
+		this.zip= zip;
+		this.phoneNo= phoneNo;
+		this.emailId= emailId;
+	}
+	public  void addContact() {
+		System.out.println( firstName+" "+ lastName+" "+address+" "+ city+" "+state+" "+ zip+" "+ phoneNo+" "+ emailId);
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book Programe");
 
+		AddressBook addressbook=new AddressBook("rina","sawant","miryabander","Ratnagiri","maharastra",123456,986543210,"abc@mail.com");
+	
+		addressbook.addContact();
 	}
 
 }
