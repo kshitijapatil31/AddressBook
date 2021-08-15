e0aeac58c3a516c960f8873e1f5ef1fb99350da6
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-15 15:41:20 +0530

    [kshitija]: first usecase solved

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -2,6 +2,15 @@ package com.bridgelabz.addressbook;
 
 public class AddressBook {
 
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	String zip;
+	int phoneNo;
+	String emailId;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book Programe");
 
